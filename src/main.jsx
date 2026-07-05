522071b247a2c5b282ade3bbab170eab5f5e49a6
varun Badami <badamivarun0@gmail.com>
2026-07-05 17:13:48 +0530

    update relese

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
-import Program from "./pages/Program.jsx";
+import Program from "./pages/program.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
